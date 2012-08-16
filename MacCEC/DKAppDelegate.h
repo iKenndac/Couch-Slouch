e162447790c61a02ae0a284a98389c6d93c3666b
Daniel Kennett <github@ikennd.ac>
2012-08-16 21:28:58 +0200

    Initial Commit

---
+++ MacCEC/DKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DKAppDelegate.h
+//  MacCEC
+//
+//  Created by Daniel Kennett on 16/08/2012.
+//  Copyright (c) 2012 Daniel Kennett. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DKAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MacCEC/DKAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  DKAppDelegate.m
+//  MacCEC
+//
+//  Created by Daniel Kennett on 16/08/2012.
+//  Copyright (c) 2012 Daniel Kennett. All rights reserved.
+//
+
+#import "DKAppDelegate.h"
+
+@implementation DKAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
