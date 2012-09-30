54aa8179968dffd8e2d2acb5330f243901e53eb6
Daniel Kennett <github@ikennd.ac>
2012-09-30 11:23:10 +0200

    Login helper no longer triggers window showing

--- src/LoginHelper-main.m
+++ src/LoginHelper-main.m
@@ -13,6 +13,10 @@ int main(int argc, char *argv[])
 	NSString *appPath = [[[[[[NSBundle mainBundle] bundlePath] stringByDeletingLastPathComponent] stringByDeletingLastPathComponent] stringByDeletingLastPathComponent] stringByDeletingLastPathComponent];
 	// This string takes you from MyGreat.App/Contents/Library/LoginItems/MyHelper.app to MyGreat.App This is an obnoxious but dynamic way to do this since that specific Subpath is required
 	NSString *binaryPath = [[NSBundle bundleWithPath:appPath] executablePath]; // This gets the binary executable within your main application
-	[[NSWorkspace sharedWorkspace] launchApplication:binaryPath];
+
+	[[NSWorkspace sharedWorkspace] launchApplicationAtURL:[NSURL fileURLWithPath:binaryPath]
+												  options:NSWorkspaceLaunchWithoutActivation
+											configuration:@{ NSWorkspaceLaunchConfigurationArguments : @[@"launchedAtLogin"] }
+													error:nil];
 	return 0;
 }
